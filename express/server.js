f67ab75bb4a2f1d5530162d704e550d75636f576
onbookdbs <92363442+onbookdbs@users.noreply.github.com>
2021-10-13 11:48:03 +0700

    Update server.js

--- express/server.js
+++ express/server.js
@@ -3,14 +3,11 @@ const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
-const bodyParser = require('body-parser');
-const router = express.Router();
-
 // Link to views folder.
 let views = path.join(__dirname, '../');
 
 // Home route.
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
     let hostName = req.headers['x-forwarded-proto'] + "://" + req.headers.host;
     res.header('Content-Type', 'application/xml');
     let url = "https://db.blitarkab.go.id/id/";
@@ -27,7 +24,7 @@ router.get('/', (req, res) => {
 });
 
 // Other routes.
-router.get('/sitemap.xml', function(req, res){
+app.get('/sitemap.xml', function(req, res){
     let hostName = req.headers['x-forwarded-proto'] + "://" + req.headers.host;
     res.header('Content-Type', 'application/xml');
     let url = "https://db.blitarkab.go.id/id/";
@@ -42,32 +39,6 @@ router.get('/sitemap.xml', function(req, res){
         res.write("</urlset>"), res.end();
     });
 });
-router.get('/page1', function(req, res){
-    let hostName = req.headers['x-forwarded-proto'] + "://" + req.headers.host;
-    res.header('Content-Type', 'application/xml');
-    let url = "https://db.blitarkab.go.id/id/";
-    let settings = { method: "Get" };
-    fetch(url, settings)
-    .then(res => res.json())
-    .then((json) => {
-        res.write('<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="'+ hostName + '/main-sitemap.xsl"?>\n  <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n');
-        json.forEach(function(json) {
-            res.write(" <url>\n"), res.write("   <loc>" + hostName+'/?id=A4999'+json + "</loc>\n"), res.write("   <lastmod>" + (new Date).toISOString() + "</lastmod>\n"), res.write(" </url>\n")
-        }),
-        res.write("</urlset>"), res.end();
-    });
-});
-router.get('/page2', function(req, res){
-  res.sendFile('page2.html', { root: views });
-});
-router.get('/page3', function(req, res){
-  res.sendFile('page3.html', { root: views });
-});
-router.get('/page4', function(req, res){
-  res.sendFile('page4.html', { root: views });
-});
-
-
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda (express/server.js)
 
