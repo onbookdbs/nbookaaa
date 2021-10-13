d0880326d0e36027f3a0a46d41e927827d8f070f
onbookdbs <92363442+onbookdbs@users.noreply.github.com>
2021-10-13 12:59:47 +0700

    Update server.js

--- express/server.js
+++ express/server.js
@@ -1,46 +1,29 @@
-'use strict';
-const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
-const app = express();
-// Link to views folder.
-let views = path.join(__dirname, '../');
+"use strict"
+const express = require("express")
+const serverless = require("serverless-http")
+const app = express()
+const bodyParser = require("body-parser")
+const router = express.Router()
 
-// Home route.
-app.get('/', (req, res) => {
-    let hostName = req.headers['x-forwarded-proto'] + "://" + req.headers.host;
-    res.header('Content-Type', 'application/xml');
-    let url = "https://db.blitarkab.go.id/id/";
-    let settings = { method: "Get" };
-    fetch(url, settings)
-    .then(res => res.json())
-    .then((json) => {
-        res.write('<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="'+ hostName + '/main-sitemap.xsl"?>\n  <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n');
-        json.forEach(function(json) {
-            res.write(" <url>\n"), res.write("   <loc>" + hostName+'/?id=A4999'+json + "</loc>\n"), res.write("   <lastmod>" + (new Date).toISOString() + "</lastmod>\n"), res.write(" </url>\n")
-        }),
-        res.write("</urlset>"), res.end();
-    });
-});
+app.use(bodyParser.json())
+app.use("/.netlify/functions/server", router) // path must route to lambda
+app.use("/", router)
 
-// Other routes.
-app.get('/sitemap.xml', function(req, res){
-    let hostName = req.headers['x-forwarded-proto'] + "://" + req.headers.host;
-    res.header('Content-Type', 'application/xml');
-    let url = "https://db.blitarkab.go.id/id/";
-    let settings = { method: "Get" };
-    fetch(url, settings)
-    .then(res => res.json())
-    .then((json) => {
-        res.write('<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="'+ hostName + '/main-sitemap.xsl"?>\n  <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n');
-        json.forEach(function(json) {
-            res.write(" <url>\n"), res.write("   <loc>" + hostName+'/?id=A4999'+json + "</loc>\n"), res.write("   <lastmod>" + (new Date).toISOString() + "</lastmod>\n"), res.write(" </url>\n")
-        }),
-        res.write("</urlset>"), res.end();
-    });
-});
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda (express/server.js)
+router.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" })
+  res.write("<h1>Up and running</h1>")
+  res.end()
+})
 
-module.exports = app;
-module.exports.handler = serverless(app);
+router.post("/doSomething", async (req, res) => {
+  try {
+    // maybe do some database interaction or third-party API call here!
+    res.status(200).send({ data: "success" })
+  } catch (err) {
+    console.log(err)
+    res.status(400).send({ error: "bad request" })
+  }
+})
+
+module.exports = app
+module.exports.handler = serverless(app)
