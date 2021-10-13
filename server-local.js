6ade418492b7270bb4d372c44c0bfef09f36bca8
onbookdbs <92363442+onbookdbs@users.noreply.github.com>
2021-10-13 12:09:15 +0700

    Update server-local.js

--- server-local.js
+++ server-local.js
@@ -1,45 +1,21 @@
-'use strict';
-const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
-const app = express();
-app.get('/sitemap.xml', function(req, res){
-        let hostName = req.headers['x-forwarded-proto'] + "://" + req.headers.host;
-        res.header('Content-Type', 'application/xml');
-        let url = "https://db.blitarkab.go.id/id/";
-        let settings = { method: "Get" };
-        fetch(url, settings)
-        .then(res => res.json())
-        .then((json) => {
-                res.write('<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="'+ hostName + '/main-sitemap.xsl"?>\n  <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n');
-                json.forEach(function(json) {
-                        res.write(" <url>\n"), res.write("   <loc>" + hostName+'/?id=A4999'+json + "</loc>\n"), res.write("   <lastmod>" + (new Date).toISOString() + "</lastmod>\n"), res.write(" </url>\n")
-                }),
-                res.write("</urlset>"), res.end();
-        });
-});
+var http = require("http");
 
-// Other routes.
-app.get('/sitemap.xml', function(req, res){
-        let hostName = req.headers['x-forwarded-proto'] + "://" + req.headers.host;
-        res.header('Content-Type', 'application/xml');
-        let url = "https://db.blitarkab.go.id/id/";
-        let settings = { method: "Get" };
-        fetch(url, settings)
-        .then(res => res.json())
-        .then((json) => {
-                res.write('<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="'+ hostName + '/main-sitemap.xsl"?>\n  <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n');
-                json.forEach(function(json) {
-                        res.write(" <url>\n"), res.write("   <loc>" + hostName+'/?id=A4999'+json + "</loc>\n"), res.write("   <lastmod>" + (new Date).toISOString() + "</lastmod>\n"), res.write(" </url>\n")
-                }),
-                res.write("</urlset>"), res.end();
-        });
-});
-module.exports = app;
-module.exports.handler = serverless(app);
+http.createServer(function(req, res) {
+  // Homepage
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Welcome to the homepage!");
+  }
 
-// Start Server.
-let port = 3005;
-app.listen(port, function(){
-    console.log(`Server started on port ${port}...`);
-});
+  // About page
+  else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Welcome to the about page!");
+  }
+
+  // 404'd!
+  else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 error! File not found.");
+  }
+}).listen(1337, "localhost");
