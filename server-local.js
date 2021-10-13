ca7907fea35d7145980248db11e5f88c9dbea32d
onbookdbs <92363442+onbookdbs@users.noreply.github.com>
2021-10-13 13:22:05 +0700

    Update server-local.js

--- server-local.js
+++ server-local.js
@@ -13,10 +13,12 @@ app.get('/', (req, res) => {
 });
 
 // Route Files.
+let sitemap = require('./routes/sitemap');
 let page1 = require('./routes/page1');
 let page2 = require('./routes/page2');
 let page3 = require('./routes/page3');
 let page4 = require('./routes/page4');
+app.use('/sitemap',sitemap);
 app.use('/page1', page1);
 app.use('/page2', page2);
 app.use('/page3', page3);
